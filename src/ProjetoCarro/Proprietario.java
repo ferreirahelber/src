7337f6c1dab4f842723fc41a870c7ea7ec37fa39
ferreirahelber <47435752+ferreirahelber@users.noreply.github.com>
2019-04-11 12:02:21 -0300

    Update Proprietario.java

--- src/ProjetoCarro/Proprietario.java
+++ src/ProjetoCarro/Proprietario.java
@@ -2,10 +2,8 @@ package ProjetoCarro;
 
 public class Proprietario {
     public static void main (String [] args){
-        String nome, cpf, rua, bairro, cidade, estado ,complemento;
-        int rg,dataDeNascimento, cep;
-        boolean
-
+        String nome, cpf, rg, rua, bairro, cidade, estado ,complemento;
+        
     }
 
 
